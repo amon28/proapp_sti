6bfb02ea469d331c6cec3f330708547b394bf2d7
amon28 <richmondgerez@gmail.com>
2024-03-07 15:12:28 +0800

    Added more details to register, added confirm password, added fullname.

--- app/src/main/java/com/example/proapp/MainActivity.java
+++ app/src/main/java/com/example/proapp/MainActivity.java
@@ -35,6 +35,9 @@ public class MainActivity extends AppCompatActivity {
     private CardView cView;
     private EditText email_text;
     private EditText password_text;
+    private EditText confirm_password_text;
+    private EditText student_number_text;
+    private EditText full_name_text;
 
     NetworkChangeListener networkChangeListener = new NetworkChangeListener();
 
@@ -57,51 +60,19 @@ public class MainActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
 
-
-        //Setup margin for card view containing login
-        RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) cView.getLayoutParams();
-        int currentMarginBottom = params.bottomMargin;
-        params.setMargins(0,0,0,-1020);
-        cView.setLayoutParams(params);
-
-        //Animate card view to go up
-
-        new CountDownTimer(100,10){
-            public void onTick(long millisUntilFinished){}
-            public void onFinish(){
-                new CountDownTimer(1000,10){
-                    public void onTick(long x){
-                        RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) cView.getLayoutParams();
-                        int bottomValue = params.bottomMargin;
-                        if(bottomValue < currentMarginBottom){
-                            params.setMargins(0,0,0,bottomValue+21);
-                            cView.setLayoutParams(params);
-                        }
-                    }
-
-                    public void onFinish(){}
-                }.start();
-
-            }
-        }.start();
+        animate_login();
 
         //Check for internet
         IntentFilter intentFilter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
         registerReceiver(networkChangeListener, intentFilter);
 
-        /*
-        ConnectivityManager connection = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo activeNetwork = connection.getActiveNetworkInfo();
-        boolean hasInternet = activeNetwork != null && activeNetwork.isConnectedOrConnecting();
-        if(!hasInternet){
-            Toast.makeText(MainActivity.this, "Need Internet Connection!", Toast.LENGTH_SHORT).show();
-            return;
-        }*/
 
         //Check if user is logged in
         FirebaseUser currentUser = mAuth.getCurrentUser();
         if(currentUser != null && currentUser.isEmailVerified()){
             Toast.makeText(MainActivity.this, "Already Login", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(MainActivity.this, Student_Activity.class));
+            finish();
         }
 
         //Event Listener for login button
@@ -116,70 +87,37 @@ public class MainActivity extends AppCompatActivity {
         unregisterReceiver(networkChangeListener);
     }
 
-    public void register_views(){
-        RegisterEmailButton = (Button) findViewById(R.id.register_email_button);
-        RegisterEmailButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v){
-            login_layout.removeAllViews();
-            login_layout.addView(View.inflate(MainActivity.this, R.layout.register_sub_activity_main, null));
+    public void animate_login(){
+        //Setup margin for card view containing login
+        RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) cView.getLayoutParams();
+        int currentMarginBottom = params.bottomMargin;
+        params.setMargins(0,0,0,-1020);
+        cView.setLayoutParams(params);
 
-            ImageButton back_button = (ImageButton) findViewById(R.id.back_button);
-            Button register_button = (Button) findViewById(R.id.register_button);
-            email_text = (EditText) findViewById(R.id.register_email_input_text);
-            password_text = (EditText) findViewById(R.id.register_password_input_text);
+        //Animate card view to go up
 
-            mAuth = FirebaseAuth.getInstance();
-
-            back_button.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    login_layout.removeAllViews();
-                    login_layout.addView(View.inflate(MainActivity.this,R.layout.login_sub_activity_main, null));
-                    login_views();
-                }
-            });
-
-
-            register_button.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    String register_email = email_text.getText().toString();
-                    String register_password = password_text.getText().toString();
-
-                    if(register_email.matches("") || register_password.matches("")){
-                        Toast.makeText(MainActivity.this, "Empty Email or Password", Toast.LENGTH_SHORT).show();
-                        return;
+        new CountDownTimer(100,10){
+            public void onTick(long millisUntilFinished){}
+            public void onFinish(){
+                new CountDownTimer(5000,10){
+                    public void onTick(long x){
+                        RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) cView.getLayoutParams();
+                        int bottomValue = params.bottomMargin;
+                        if(bottomValue < currentMarginBottom){
+                            params.setMargins(0,0,0,bottomValue+21);
+                            cView.setLayoutParams(params);
+                        }else{
+                           this.cancel();
+                        }
                     }
 
-                    Task<AuthResult> createUser = mAuth.createUserWithEmailAndPassword(register_email, register_password);
+                    public void onFinish(){}
+                }.start();
 
-                    createUser.addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()) {
-                                // Sign in success, update UI with the signed-in user's information
-                                FirebaseUser user = mAuth.getCurrentUser();
-                                user.sendEmailVerification();
-                                Toast.makeText(MainActivity.this, "Register Success!", Toast.LENGTH_SHORT).show();
-                                login_views();
-                            } else {
-                                // If sign in fails, display a message to the user.
-                                Toast.makeText(MainActivity.this, "User Already Exist!.", Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                    });
-                    createUser.addOnFailureListener(MainActivity.this, new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Toast.makeText(MainActivity.this, "Failed To Register", Toast.LENGTH_SHORT).show();
-                        }
-                    });
-                }
-            });
             }
-        });
+        }.start();
     }
+
     public void login_views(){
         login_layout.removeAllViews();
         login_layout.addView(View.inflate(MainActivity.this,R.layout.login_sub_activity_main, null));
@@ -229,6 +167,78 @@ public class MainActivity extends AppCompatActivity {
         });
 
         //Event listener for register button
-        register_views();
+        RegisterEmailButton = (Button) findViewById(R.id.register_email_button);
+        RegisterEmailButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                login_layout.removeAllViews();
+                login_layout.addView(View.inflate(MainActivity.this, R.layout.register_sub_activity_main, null));
+
+                ImageButton back_button = (ImageButton) findViewById(R.id.back_button);
+                Button register_button = (Button) findViewById(R.id.register_button);
+                email_text = (EditText) findViewById(R.id.register_email_input_text);
+                password_text = (EditText) findViewById(R.id.register_password_input_text);
+                confirm_password_text = (EditText) findViewById(R.id.register_confirm_password_input_text);
+                student_number_text = (EditText) findViewById(R.id.register_snumber_input_text);
+                full_name_text = (EditText) findViewById(R.id.register_name_input_text);
+
+                mAuth = FirebaseAuth.getInstance();
+
+                back_button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        login_layout.removeAllViews();
+                        login_layout.addView(View.inflate(MainActivity.this,R.layout.login_sub_activity_main, null));
+                        login_views();
+                    }
+                });
+
+
+                register_button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String register_email = email_text.getText().toString();
+                        String register_password = password_text.getText().toString();
+                        String confirm_password = confirm_password_text.getText().toString();
+                        String student_number = student_number_text.getText().toString();
+                        String full_name = full_name_text.getText().toString();
+
+                        if(register_email.matches("") || register_password.matches("")){
+                            Toast.makeText(MainActivity.this, "Empty Email or Password", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+
+                        if(!confirm_password.matches(register_password)){
+                            Toast.makeText(MainActivity.this, "Password Must Be The Same.", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+
+                        Task<AuthResult> createUser = mAuth.createUserWithEmailAndPassword(register_email, register_password);
+
+                        createUser.addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    user.sendEmailVerification();
+                                    Toast.makeText(MainActivity.this, "Register Success!", Toast.LENGTH_SHORT).show();
+                                    login_views();
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(MainActivity.this, "User Already Exist!.", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                        createUser.addOnFailureListener(MainActivity.this, new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(MainActivity.this, "Failed To Register", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                });
+            }
+        });
     }
 }
